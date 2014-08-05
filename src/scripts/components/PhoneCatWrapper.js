1fad55180a93e418733e0e1bc145e162be3cfcf4
Girish S <girish.sonawane@gmail.com>
2014-08-05 21:30:08 +0530

    step-2 replace class with className

--- src/scripts/components/PhoneCatWrapper.js
+++ src/scripts/components/PhoneCatWrapper.js
@@ -3,8 +3,8 @@
 var PhoneCatWrapper = React.createClass({
   render: function() {
     return (
-      <div class="row">
-        <div class="col-md-2">
+      <div className="row">
+        <div className="col-md-2">
           Search:
           <input type="text"/>
           Sort by:
@@ -13,26 +13,26 @@ var PhoneCatWrapper = React.createClass({
             <option value="age">Newest</option>
           </select>
         </div>
-        <div class="col-md-10">
-          <ul class="phones">
-          <li class="thumbnail phone-listing">
-            <a href="#/phones/motorola-xoom-with-wi-fi" class="thumb"><img src="/images/phones/motorola-xoom-with-wi-fi.0.jpg" /></a>
+        <div className="col-md-10">
+          <ul className="phones">
+          <li className="thumbnail phone-listing">
+            <a href="#/phones/motorola-xoom-with-wi-fi" className="thumb"><img src="images/phones/motorola-xoom-with-wi-fi.0.jpg" /></a>
             <a href="#/phones/motorola-xoom-with-wi-fi" >Motorola XOOM™ with Wi-Fi</a>
             <p>The Next, Next Generation
 
               Experience the future with Motorola XOOM with Wi-Fi, the world's first tablet powered by Android 3.0 (Honeycomb).
             </p>
           </li>
-          <li class="thumbnail phone-listing">
-            <a href="#/phones/motorola-xoom-with-wi-fi" class="thumb"><img src="/images/phones/motorola-xoom-with-wi-fi.0.jpg" /></a>
+          <li className="thumbnail phone-listing">
+            <a href="#/phones/motorola-xoom-with-wi-fi" className="thumb"><img src="images/phones/motorola-xoom-with-wi-fi.0.jpg" /></a>
             <a href="#/phones/motorola-xoom-with-wi-fi" >Motorola XOOM™ with Wi-Fi</a>
             <p>The Next, Next Generation
 
               Experience the future with Motorola XOOM with Wi-Fi, the world's first tablet powered by Android 3.0 (Honeycomb).
             </p>
           </li>
-          <li class="thumbnail phone-listing">
-            <a href="#/phones/motorola-xoom-with-wi-fi" class="thumb"><img src="/images/phones/motorola-xoom-with-wi-fi.0.jpg" /></a>
+          <li className="thumbnail phone-listing">
+            <a href="#/phones/motorola-xoom-with-wi-fi" className="thumb"><img src="images/phones/motorola-xoom-with-wi-fi.0.jpg" /></a>
             <a href="#/phones/motorola-xoom-with-wi-fi" >Motorola XOOM™ with Wi-Fi</a>
             <p>The Next, Next Generation
 
