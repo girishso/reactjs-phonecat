06b0497647b487f648af9764022151ab503c4a9a
Girish S <girish.sonawane@gmail.com>
2014-08-06 19:49:05 +0530

    got rid of everything

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-/** @jsx React.DOM */
-
-// Bring in jQuery and React as a Bower component in the global namespace
-require("script!react/react-with-addons.js")
-require("script!jquery/jquery.js")
-
-var PhoneCatWrapper = require("./components/PhoneCatWrapper.js")
-
-React.renderComponent(<PhoneCatWrapper />, document.getElementById('app'))
