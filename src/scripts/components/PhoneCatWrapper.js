cf209ea1d2ef52432e6c84937a22ad2a08083891
Girish S <girish.sonawane@gmail.com>
2014-08-06 18:30:33 +0530

    step-6 added ajax

--- src/scripts/components/PhoneCatWrapper.js
+++ src/scripts/components/PhoneCatWrapper.js
@@ -1,42 +1,23 @@
 /** @jsx React.DOM */
 var PhoneCat = require("./PhoneCat.js")
 
-var PHONES = [
-    {
-        "age": 0,
-        "id": "motorola-xoom-with-wi-fi",
-        "imageUrl": "images/phones/motorola-xoom-with-wi-fi.0.jpg",
-        "name": "Motorola XOOM\u2122 with Wi-Fi",
-        "snippet": "The Next, Next Generation\r\n\r\nExperience the future with Motorola XOOM with Wi-Fi, the world's first tablet powered by Android 3.0 (Honeycomb)."
-    },
-    {
-        "age": 1,
-        "id": "motorola-xoom",
-        "imageUrl": "images/phones/motorola-xoom.0.jpg",
-        "name": "MOTOROLA XOOM\u2122",
-        "snippet": "The Next, Next Generation\n\nExperience the future with MOTOROLA XOOM, the world's first tablet powered by Android 3.0 (Honeycomb)."
-    },
-    {
-        "age": 2,
-        "carrier": "AT&T",
-        "id": "motorola-atrix-4g",
-        "imageUrl": "images/phones/motorola-atrix-4g.0.jpg",
-        "name": "MOTOROLA ATRIX\u2122 4G",
-        "snippet": "MOTOROLA ATRIX 4G the world's most powerful smartphone."
-    },
-    {
-        "age": 3,
-        "id": "dell-streak-7",
-        "imageUrl": "images/phones/dell-streak-7.0.jpg",
-        "name": "Dell Streak 7",
-        "snippet": "Introducing Dell\u2122 Streak 7. Share photos, videos and movies together. It\u2019s small enough to carry around, big enough to gather around."
-    }
-]
-
 var PhoneCatWrapper = React.createClass({
+  getInitialState: function() {
+      return {
+        phones: []
+      }
+  },
+
+  componentDidMount: function() {
+    $.getJSON('phones/phones.json', (function(data) {
+      this.setState({phones: data});
+    }).bind(this));
+
+  },
+
   render: function() {
     return (
-      <PhoneCat phones={PHONES} />
+      <PhoneCat phones={this.state.phones} />
     )
   }
 });
