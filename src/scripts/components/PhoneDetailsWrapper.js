0fb57ca800a02dd3c672f6f85b68fd0e5ee37a21
Girish S <girish.sonawane@gmail.com>
2014-08-08 16:55:02 +0530

    step-9 added ajax in PhoneDetailsWrapper

--- src/scripts/components/PhoneDetailsWrapper.js
+++ src/scripts/components/PhoneDetailsWrapper.js
@@ -2,11 +2,42 @@
 var PhoneDetails = require("./PhoneDetails.js")
 
 var PhoneDetailsWrapper = React.createClass({
+  getInitialState: function() {
+    return {
+      phone: {
+        images: [],
+        battery: {},
+        storage: {},
+        connectivity: {},
+        android: {},
+        sizeAndWeight: {
+          dimensions: []
+        },
+        display: {},
+        hardware: {},
+        camera: {
+          features: []
+        }
+      }
+    };
+  },
+
+
+  componentDidMount: function() {
+    var url;
+    url = "phones/" + this.props.params.phoneId + ".json";
+    return $.getJSON(url, (function(data) {
+      data.phone_image = data.images[0];
+      return this.setState({
+        phone: data
+      });
+    }).bind(this));
+  },
+
   render: function() {
-    return (
-      <PhoneDetails />
-    )
+    return <PhoneDetails phone={this.state.phone} />
   }
+
 });
 
 module.exports = PhoneDetailsWrapper;
