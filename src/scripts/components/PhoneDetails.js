eaadb7da9c3542298da2132b3437bdbf5894d0e3
Girish S <girish.sonawane@gmail.com>
2014-08-08 17:03:24 +0530

    step-10 use props.phone in PhoneDetails

--- src/scripts/components/PhoneDetails.js
+++ src/scripts/components/PhoneDetails.js
@@ -1,135 +1,145 @@
 /** @jsx React.DOM */
-var ImageGallery = require("./ImageGallery.js")
+var ImageGallery = require("./ImageGallery.js");
 
 var PhoneDetails = React.createClass({
+  checkmark: function(truthy) {
+    if (truthy) {
+      return String.fromCharCode(10003);
+    } else {
+      return String.fromCharCode(10008);
+    }
+  },
+
   render: function() {
+    var phone = this.props.phone;
+    var dimensions = phone.sizeAndWeight.dimensions.map(function(dimension, i) {
+      return <dd key={i}>{ dimension }</dd>;
+    });
+    
     return (
-      <section id="main"><div className="phone-images"><img src="images/phones/motorola-xoom-with-wi-fi.3.jpg" className="phone" />
-      </div>
-      <h1>Motorola XOOM™ with Wi-Fi</h1>
-      <p>Motorola XOOM with Wi-Fi has a super-powerful dual-core processor and Android™ 3.0 (Honeycomb) — the Android platform designed specifically for tablets. With its 10.1-inch HD widescreen display, you’ll enjoy HD video in a thin, light, powerful and upgradeable tablet.</p>
+      <section id="main">
+        <div className="phone-images">
+          <img ref="phone_image" src={ this.props.phone.images[0]} className="phone" />
+        </div>
+        <h1>{phone.name}</h1>
+        <p>{phone.description}</p>
 
-      <ImageGallery />
+        <ImageGallery />
 
-      <ul className="specs"><li>
-          <span>Availability and Networks</span>
-          <dl>
-              <dt>Availability</dt>
-              <dd></dd>
-          </dl>
-      </li>
+        <ul className="specs">
+          <li>
+              <span>Availability and Networks</span>
+              <dl>
+                  <dt>Availability</dt>
+                  <dd>{ phone.availability }</dd>
+              </dl>
+          </li>
 
-      <li>
-          <span>Battery</span>
-          <dl>
-              <dt>Type</dt>
-              <dd>Other ( mAH)</dd>
-              <dt>Talk Time</dt>
-              <dd>24 hours</dd>
-              <dt>Standby time (max)</dt>
-              <dd>336 hours</dd>
-          </dl>
-      </li>
+          <li>
+              <span>Battery</span>
+              <dl>
+                  <dt>Type</dt>
+                  <dd>{ phone.battery.type }</dd>
+                  <dt>Talk Time</dt>
+                  <dd>{ phone.battery.talkTime }</dd>
+                  <dt>Standby time (max)</dt>
+                  <dd>{ phone.battery.standbyTime }</dd>
+              </dl>
+          </li>
 
-      <li>
-          <span>Storage and Memory</span>
-          <dl>
-              <dt>RAM</dt>
-              <dd>1000MB</dd>
-              <dt>Internal Storage</dt>
-              <dd>32000MB</dd>
-          </dl>
-      </li>
+          <li>
+              <span>Storage and Memory</span>
+              <dl>
+                  <dt>RAM</dt>
+                  <dd>{ phone.storage.ram }</dd>
+                  <dt>Internal Storage</dt>
+                  <dd>{ phone.storage.flash }</dd>
+              </dl>
+          </li>
 
-      <li>
-          <span>Connectivity</span>
-          <dl>
-              <dt>Network Support</dt>
-              <dd></dd>
-              <dt>WiFi</dt>
-              <dd>802.11 b/g/n</dd>
-              <dt>Bluetooth</dt>
-              <dd>Bluetooth 2.1</dd>
-              <dt>Infrared</dt>
-              <dd>✘</dd>
-              <dt>GPS</dt>
-              <dd>✓</dd>
-          </dl>
-      </li>
+          <li>
+              <span>Connectivity</span>
+              <dl>
+                  <dt>Network Support</dt>
+                  <dd>{ phone.connectivity.cell }</dd>
+                  <dt>WiFi</dt>
+                  <dd>{ phone.connectivity.wifi }</dd>
+                  <dt>Bluetooth</dt>
+                  <dd>{ phone.connectivity.bluetooth }</dd>
+                  <dt>Infrared</dt>
+                  <dd>{ this.checkmark(phone.connectivity.infrared) }</dd>
+                  <dt>GPS</dt>
+                  <dd>{ this.checkmark(phone.connectivity.gps) }</dd>
+              </dl>
+          </li>
 
-      <li>
-          <span>Android</span>
-          <dl>
-              <dt>OS Version</dt>
-              <dd>Android 3.0</dd>
-              <dt>UI</dt>
-              <dd>Honeycomb</dd>
-          </dl>
-      </li>
+          <li>
+              <span>Android</span>
+              <dl>
+                  <dt>OS Version</dt>
+                  <dd>{ phone.android.os }</dd>
+                  <dt>UI</dt>
+                  <dd>{ phone.android.ui }</dd>
+              </dl>
+          </li>
 
-      <li>
-          <span>Size and Weight</span>
-          <dl>
-              <dt>Dimensions</dt>
+          <li>
+              <span>Size and Weight</span>
+              <dl>
+                  <dt>Dimensions</dt>
+                    { dimensions }
+                  <dt>Weight</dt>
+                  <dd>{ phone.sizeAndWeight.weight }</dd>
+              </dl>
+          </li>
 
-                  <dd>249.1 mm (w)</dd>
+          <li>
+              <span>Display</span>
+              <dl>
+                  <dt>Screen size</dt>
+                  <dd>{ phone.display.screenSize }</dd>
+                  <dt>Screen resolution</dt>
+                  <dd>{ phone.display.screenResolution }</dd>
+                  <dt>Touch screen</dt>
+                  <dd>{ this.checkmark(phone.display.touchScreen) }</dd>
+              </dl>
+          </li>
 
-                  <dd>167.8 mm (h)</dd>
+          <li>
+              <span>Hardware</span>
+              <dl>
+                  <dt>CPU</dt>
+                  <dd>{ phone.hardware.cpu }</dd>
+                  <dt>USB</dt>
+                  <dd>{ phone.hardware.usb }</dd>
+                  <dt>Audio / headphone jack</dt>
+                  <dd>{ phone.hardware.audioJack }</dd>
+                  <dt>FM Radio</dt>
+                  <dd> { this.checkmark(phone.hardware.fmRadio) }</dd>
+                  <dt>Accelerometer</dt>
+                  <dd>{ this.checkmark(phone.hardware.accelerometer) }</dd>
+              </dl>
+          </li>
 
-                  <dd>12.9 mm (d)</dd>
+          <li>
+              <span>Camera</span>
+              <dl>
+                  <dt>Primary</dt>
+                  <dd>{ phone.camera.primary }</dd>
+                  <dt>Features</dt>
+                  <dd>{ phone.camera.features.join(', ') }</dd>
+              </dl>
+          </li>
 
-              <dt>Weight</dt>
-              <dd>708.0 grams</dd>
-          </dl>
-      </li>
+          <li>
+              <span>Additional Features</span>
+              <dd>{ phone.additionalFeatures }</dd>
+          </li>
 
-      <li>
-          <span>Display</span>
-          <dl>
-              <dt>Screen size</dt>
-              <dd>10.1 inches</dd>
-              <dt>Screen resolution</dt>
-              <dd>WXGA (1200 x 800)</dd>
-              <dt>Touch screen</dt>
-              <dd>✓</dd>
-          </dl>
-      </li>
-
-      <li>
-          <span>Hardware</span>
-          <dl>
-              <dt>CPU</dt>
-              <dd>1 GHz Dual Core Tegra 2</dd>
-              <dt>USB</dt>
-              <dd>USB 2.0</dd>
-              <dt>Audio / headphone jack</dt>
-              <dd>3.5mm</dd>
-              <dt>FM Radio</dt>
-              <dd>✘</dd>
-              <dt>Accelerometer</dt>
-              <dd>✓</dd>
-          </dl>
-      </li>
-
-      <li>
-          <span>Camera</span>
-          <dl>
-              <dt>Primary</dt>
-              <dd>5.0 megapixels</dd>
-              <dt>Features</dt>
-              <dd>Flash, Video</dd>
-          </dl>
-      </li>
-
-      <li>
-          <span>Additional Features</span>
-          <dd>Sensors: proximity, ambient light, barometer, gyroscope</dd>
-      </li>
-
-      <li></li>
-      </ul>
+          <li></li>
+        </ul>
       </section>
-    )
+    );
   }
 });
 
